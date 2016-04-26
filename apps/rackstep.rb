2e7b380bdc8f06c1f3a69777ef9cea1363cab803
Marcio Frayze David <mfdavid@gmail.com>
2016-04-26 16:37:22 -0300

    Adding RackStep framework

---
+++ apps/rackstep.rb
@@ -0,0 +1,13 @@
+require 'rackstep'
+
+class HelloWorld < RackStep::App
+  add_route('GET', '', 'HelloWorldController')
+end
+
+class HelloWorldController < RackStep::Controller
+  def process_request
+    response.body = 'Hello World!'
+  end
+end
+
+run HelloWorld
